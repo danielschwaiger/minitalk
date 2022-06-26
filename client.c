07327ff13811efd7937f3d7743fd0ded201dd7a4
Daniel Panciuc <danielcccasle@gmail.com>
2022-06-26 22:15:54 +0200

    updates client and server - Tofix bits loss on sending multiple messages

--- client.c
+++ client.c
@@ -6,24 +6,56 @@
 /*   By: dapanciu <dapanciu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 13:13:48 by dapanciu          #+#    #+#             */
-/*   Updated: 2022/06/20 18:21:49 by dapanciu         ###   ########.fr       */
+/*   Updated: 2022/06/26 21:04:49 by dapanciu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include <sys/wait.h>
 #include <signal.h>
 #include "minitalk.h"
 #include "libft.h"
 #include "ft_printf.h"
 
-int main(void)
+//TODO implement malloc !! 
+// TODO implement a function to print-out the received string !! GNL ??? 
+// pass the norminette !! 
+
+void	send_signal(unsigned char c, int pid)
 {
-	ft_printf("Hello from CLIENT side!!!\n");
-	ft_printf("CLIENT COUNT = %zu\n", ft_strlen("TESTING"));
-	ft_putnbr_fd(100, 3);
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if (c & (1 << bit))
+			kill(pid, SIGUSR1);
+			//exit(1) ?
+		else
+			kill(pid, SIGUSR2);
+			//exit(1) ?
+		usleep(10000); // look how to implement it several times .. 
+		bit++;
+	}  // how do i know when the string ended???
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	if (argc != 3)
+	{
+		ft_printf("Error! Invalid number of arguments!!\n");
+		return (0);
+	}
+	pid = ft_atoi(argv[1]);
+	if (pid == -1 || !pid || argv[2][0] == '\0')
+		return (0);
+	i = 0;
+	while (argv[2][i])
+	{
+		send_signal(argv[2][i], pid);
+		i++;
+	}
+	send_signal('\n', pid);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,23 +6,50 @@
 /*   By: dapanciu <dapanciu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 13:13:52 by dapanciu          #+#    #+#             */
-/*   Updated: 2022/06/20 18:23:41 by dapanciu         ###   ########.fr       */
+/*   Updated: 2022/06/26 20:59:41 by dapanciu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include <sys/wait.h>
 #include <signal.h>
 #include "minitalk.h"
 #include "libft.h"
 #include "ft_printf.h"
 
-int main(void)
+//TODO implement malloc !! 
+// TODO implement a function to printout the received string !! GNL ??? 
+// pass the norminette !! 
+
+static void receive_signal(int signal)
 {
-	ft_printf("Hello from SERVER side!!!\n");
-	ft_printf("len of printff = %zu\n", ft_strlen("Hello from SERVER side!!!"));
+	static int bit;
+	static int n;
+
+	if (signal == SIGUSR1)
+		n |= (1 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		// implement memset ? 
+		ft_printf("%c", n);
+		bit = 0;
+		n = 0;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	if (argc != 1)
+	{
+		ft_printf("Please, run server without any arguments!\n");
+		return (0);
+	}
+	ft_printf("Server running (PID : %d):\n", getpid());
+	while(1)
+	{
+		signal(SIGUSR1, receive_signal);
+		signal(SIGUSR2, receive_signal);
+		pause ();
+	}
 	return (0);
 }
